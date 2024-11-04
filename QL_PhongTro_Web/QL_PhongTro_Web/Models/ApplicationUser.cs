e71df89840a0f83bb237dc5b5177e42639bfd7ab
Nguyen Minh Duc <nguyenminhduc240204@gmail.com>
2024-11-04 23:20:15 +0700

    Thêm mã nguồn mới

--- QL_PhongTro_Web/QL_PhongTro_Web/Models/ApplicationUser.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity;
-
-namespace QL_PhongTro_Web.Models
-{
-    public class ApplicationUser : IdentityUser
-    {
-        
-    }
-}
--- QL_PhongTro_Web/QL_PhongTro_Web/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace QL_PhongTro_Web.Models
-{
-	public class ErrorViewModel
-	{
-		public string? RequestId { get; set; }
-
-		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-	}
-}
